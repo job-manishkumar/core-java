2fc1731d5acda21406b19cf160c4c92baea429a7
manish <job.manishkumar@gmail.com>
2023-12-30 11:47:27 +0530

    find maximum between two numbers

---
+++ basic/src/main/java/com/basic/ifelseprogram/MaximunNumberbwTwo.java
@@ -0,0 +1,35 @@
+package com.basic.ifelseprogram;
+
+import java.util.Scanner;
+
+public class MaximunNumberbwTwo {
+
+	//program to find maximum between two numbers
+	public static void main(String[] args) {
+
+		Scanner scanner =new Scanner(System.in);
+
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		//using without function
+		int maximumNumber = getMaximumNumber(a, b);
+		System.out.println("Maximum number between "+a+ "  and "+b+ " is : "+maximumNumber);
+
+		//using Math.max function
+		int greaterNumber = getGreaterNumber(a, b);
+		System.out.println("Greater number between "+a+ "  and "+b+ " is : "+greaterNumber);
+
+	}
+
+	// using without function
+	public static int getMaximumNumber(int a, int b){
+
+		return  a>b?a:b;
+	}
+
+	//using Math.max function
+	public static int getGreaterNumber(int a, int b){
+		return Math.max(a,b);
+	}
+}
