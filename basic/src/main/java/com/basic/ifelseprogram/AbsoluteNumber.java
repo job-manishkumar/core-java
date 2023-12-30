a5e5bc261d70ff1082993b04711a568c17b139e7
manish <job.manishkumar@gmail.com>
2023-12-30 13:44:22 +0530

    program to get absolute Number

---
+++ basic/src/main/java/com/basic/ifelseprogram/AbsoluteNumber.java
@@ -0,0 +1,22 @@
+package com.basic.ifelseprogram;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class AbsoluteNumber {
+
+	//program to get absolute Number
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int a = scanner.nextInt();
+
+		int absoluteNum = getAbsoluteNumber(a);
+		System.out.println("Absolute Number : "+absoluteNum);
+	}
+
+	public  static int getAbsoluteNumber(int num){
+		return  num>=0?num:-num;
+	}
+}
