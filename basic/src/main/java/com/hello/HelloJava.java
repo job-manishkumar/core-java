f8c5cfae0c9ad5390817ecba8db7286d1518968e
manish <job.manishkumar@gmail.com>
2023-12-30 11:29:23 +0530

    first commit for hello java

---
+++ basic/src/main/java/com/hello/HelloJava.java
@@ -0,0 +1,11 @@
+package com.hello;
+
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+
+		// Printing hello java string for the 1st time in java program.
+		System.out.println("Hello Java.");
+	}
+}
