23d93ae1cc280cbbdd73af06061d7daea5d15e7a
manish <job.manishkumar@gmail.com>
2024-01-01 19:52:36 +0530

    ount the number of elements in given array greater than x

---
+++ basic/src/main/java/com/basic/arrays/CountGreaterNumber.java
@@ -0,0 +1,43 @@
+package com.basic.arrays;
+
+import java.util.Scanner;
+
+public class CountGreaterNumber {
+
+	public static final int ARRAY_LENGTH = 5;
+	public static final int ELEMENT = 10;
+
+	//count the number of elements in given array greater than x.
+
+	public static void main(String[] args) {
+
+		System.out.println("enter Array element...");
+		int[] a = getInputArray(ARRAY_LENGTH);
+		int count = countGreaterNumber(a,ELEMENT);
+		System.out.println(count+ " elements are grater than "+ELEMENT);
+	}
+	public static int[] getInputArray(int length){
+
+		int[] a = new int[length];
+		Scanner scanner = new Scanner(System.in);
+		for(int i=0;i<a.length;i++){
+			a[i]=scanner.nextInt();
+		}
+			return  a;
+	}
+
+	public static int countGreaterNumber(int[] a, int element){
+
+		if(a.length==0){
+			throw  new IllegalArgumentException("Array length is zero");
+		}
+		int count=0;
+		for(int i:a){
+			if(element<i){
+				System.out.println("elements which are grater than "+element+" is :"+i);
+				count++;
+			}
+		}
+		return  count;
+	}
+}
