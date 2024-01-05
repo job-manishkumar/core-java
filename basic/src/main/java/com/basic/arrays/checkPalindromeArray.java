2934ca1e24f2410707f99b75de1be281732cc861
manish <job.manishkumar@gmail.com>
2024-01-05 11:39:52 +0530

    check given array is palindrome or not

---
+++ basic/src/main/java/com/basic/arrays/checkPalindromeArray.java
@@ -0,0 +1,45 @@
+package com.basic.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class checkPalindromeArray {
+
+
+	// check given array is palindrome or not.
+	public static void main(String[] args) {
+
+		int length = 6;
+		System.out.println("Enter Input Array...");
+		boolean checkPalindrome = getReverseArray(getInputArray(length));
+		if (checkPalindrome) {
+			System.out.println("Array is palindrome...");
+		} else {
+			System.out.println("Array is not palindrome...");
+		}
+	}
+
+	public static int[] getInputArray(int length) {
+
+		int[] a = new int[length];
+		Scanner scanner = new Scanner(System.in);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = scanner.nextInt();
+		}
+		return a;
+	}
+
+	public static boolean getReverseArray(int[] a) {
+
+		if (a.length <= 1) {
+			throw new IllegalArgumentException("Array size is zero");
+		}
+		for (int i = 0, j = a.length - 1; i < j; i++, j--) {
+			if (a[i] != a[j]) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+}
