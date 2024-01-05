45e74512d7b5a0a935a0428df2011f36e30b5167
manish <job.manishkumar@gmail.com>
2024-01-05 15:55:07 +0530

    Binary search

---
+++ basic/src/main/java/com/basic/arrays/searchingsorting/BinarySearch.java
@@ -0,0 +1,55 @@
+package com.basic.arrays.searchingsorting;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+
+	//Binary search --> array should be sorted.
+	public static void main(String[] args) {
+		int length = 8;
+		int element = 6;
+
+		int index = binarySearch(getInputSortedArray(length), element);
+		if (index == -1) {
+			System.out.println("element not found ");
+		} else {
+			System.out.println("element found at = " + index + " index");
+		}
+
+	}
+
+	public static int[] getInputSortedArray(int length) {
+
+		int[] a = new int[length];
+		Scanner scanner = new Scanner(System.in);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = scanner.nextInt();
+		}
+		return a;
+	}
+
+	public static int binarySearch(int[] a, int element) {
+
+		if (a.length == 0) {
+			throw new IllegalArgumentException("Array size is zero");
+		}
+
+		int lowerIndex = 0;
+		int highIndex = a.length - 1;
+		int mediumIndex = -1;
+
+		while (lowerIndex <= highIndex) {
+
+			mediumIndex = (lowerIndex + highIndex) / 2;
+
+			if (a[mediumIndex] == element) {
+				return mediumIndex;
+			} else if (a[mediumIndex] < element) {
+				lowerIndex = mediumIndex + 1;
+			} else {
+				highIndex = mediumIndex - 1;
+			}
+		}
+		return -1;
+	}
+}
