7adfd64f83e96ebd8caa8a137bb5e661b3936010
manish <job.manishkumar@gmail.com>
2024-01-05 18:55:00 +0530

    selection sort

---
+++ basic/src/main/java/com/basic/arrays/sorting/SelectionSort.java
@@ -0,0 +1,46 @@
+package com.basic.arrays.sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+
+	/*selects the smallest (or largest) element from the unsorted portion
+	 of the list and swaps it with the first element of the unsorted part*/
+
+
+	public static void main(String[] args) {
+
+		int length = 6;
+
+		int[] b = selectionSort(getInputArray(length));
+		System.out.println("Sort array using selection sort :" + Arrays.toString(b));
+	}
+
+	public static int[] getInputArray(int length) {
+
+		int[] a = new int[length];
+		Scanner scanner = new Scanner(System.in);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = scanner.nextInt();
+		}
+		return a;
+	}
+
+	public static int[] selectionSort(int[] a) {
+
+		if (a.length == 0) {
+			throw new IllegalArgumentException("Array size must be greater than 0");
+		}
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] > a[j]) {
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		return a;
+	}
+}
